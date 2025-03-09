0ef47ccf115aa15ecff8bcb7ee66d05582373280
eunice <atandainioluwa371@gmail.com>
2025-03-09 16:04:23 -0700

    deploy to vercel

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth"; // ✅ Import authentication module
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "creative-cue-akql.vercel.app",
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
